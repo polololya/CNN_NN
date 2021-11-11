5ae3f1855b1def978651c4dbd31f0fdf5910bb34
Maksym <maksym.loktionov@gmail.com>
2021-11-11 13:24:18 +0200

    model training added

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 import tensorflow.python.framework.config
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
-
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.models import Model
 
 class ImageClassificationTF:
     def __init__(self,path):
@@ -25,7 +27,7 @@ class ImageClassificationTF:
                 plt.imshow(images[i].numpy().astype("uint8"))
                 plt.title(int(labels[i]))
                 plt.axis("off")
-
+                plt.suptitle('Dataset visualization with labels')
 
     def test_train_val(self):
         train_datagen = ImageDataGenerator(
@@ -47,7 +49,7 @@ class ImageClassificationTF:
             class_mode='binary',
         subset='training')
 
-        validation_generator = train_datagen.flow_from_directory(
+        self.validation_generator = train_datagen.flow_from_directory(
             self.path + '/train',
             target_size=(315, 315),
             batch_size=32,
@@ -58,7 +60,7 @@ class ImageClassificationTF:
         self.test_pictures = test_datagen.flow_from_directory(self.path+'/test/', target_size=(315, 315),
                                                          batch_size=32,
                                                          class_mode='binary')
-
+        return self.train_generator, self.validation_generator, self.test_pictures
 
 
     def augumented_visualization(self):
@@ -68,20 +70,125 @@ class ImageClassificationTF:
         for img, ax in zip(imgs, axes):
             ax.imshow(img)
             ax.axis('off')
+            plt.suptitle('Examples of data augmentation')
         plt.tight_layout()
         plt.show()
 
+    def customized_model(self):
+        def make_model(input_shape, num_classes,data_augmentation):
+            inputs = keras.Input(shape= input_shape)
+            # Image augmentation block
+            x = data_augmentation(inputs)
+
+            # Entry block
+            x = layers.Rescaling(1.0 / 255)(inputs)
+            x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+            x = layers.Activation("relu")(x)
+
+            x = layers.Conv2D(64, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+            x = layers.Activation("relu")(x)
+
+            previous_block_activation = x  # Set aside residual
+
+            for self.size in [128, 256, 512, 728]:
+                x = layers.Activation("relu")(x)
+                x = layers.SeparableConv2D(self.size, 3, padding="same")(x)
+                x = layers.BatchNormalization()(x)
+
+                x = layers.Activation("relu")(x)
+                x = layers.SeparableConv2D(self.size, 3, padding="same")(x)
+                x = layers.BatchNormalization()(x)
+
+                x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+                # Project residual
+                residual = layers.Conv2D(self.size, 1, strides=2, padding="same")(
+                    previous_block_activation
+                )
+                x = layers.add([x, residual])  # Add back residual
+                previous_block_activation = x  # Set aside next residual
+
+            x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+            x = layers.Activation("relu")(x)
+
+            x = layers.GlobalAveragePooling2D()(x)
+            if num_classes == 2:
+                activation = "sigmoid"
+                units = 1
+            else:
+                activation = "softmax"
+                units = num_classes
+
+            x = layers.Dropout(0.2)(x)
+            outputs = layers.Dense(units, activation=activation)(x)
+            return keras.Model(inputs, outputs)
+
+        image_size = (315, 315)
+        data_augmentation = keras.Sequential(
+            [
+                layers.RandomFlip("horizontal_and_vertical"),  # Random horizontal and verical picture change
+                layers.RandomRotation(0.1),  # Random rotation of the picture - 10 degrees
+                layers.RandomContrast(0.003),  # Adjust contrast for each picture independantly
+                layers.RandomHeight(0.3),  # Random change height of the picture
+
+            ]
+        )
+        customized_model = make_model(input_shape=image_size + (3,), num_classes=2,data_augmentation=data_augmentation)
+
+
+        return customized_model
+
+
+    def training_process(self, model,epochs,train_ds,val_ds, test_set):
+        model.summary()  # model layers+info
+        # keras.utils.plot_model(model, show_shapes=True)#visualization of the model
+
+        # Compile model
+        model.compile(
+            optimizer=keras.optimizers.Adam(1e-3),  # most popular optimizer at the moment
+            loss="binary_crossentropy",  # loss for classification problem
+            metrics=["accuracy"],  # required metric
+        )
+
+        # Fit model with data
+        history = model.fit(train_ds, epochs=epochs,
+                            # callbacks=callbacks,
+                            validation_data=val_ds,
+                            )
+        # Model results visualization
+
+        # summarize history for accuracy
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+
+        score = model.evaluate(test_set, verbose=0)
+        print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
 
 
 
 
 
-
-data_visualization = ImageClassificationTF('hotdog__not_hotdog')
-data_visualization.test_train_val()
-data_visualization.visualization()
-data_visualization.augumented_visualization()
-
-
+hot_dog = ImageClassificationTF('hotdog__not_hotdog')
+hot_dog.test_train_val()
+#data_visualization.visualization()
+#data_visualization.augumented_visualization()
+hot_dog.customized_model()
+hot_dog.training_process(hot_dog.customized_model(),50,*hot_dog.test_train_val())
 
 
