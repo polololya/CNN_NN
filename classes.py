8370ecb2b7412e81a539015ad702c84ec8223d9e
Maksym <maksym.loktionov@gmail.com>
2021-11-14 07:26:10 +0200

    updated code+pytorch visualizations

--- classes.py
+++ classes.py
@@ -22,8 +22,8 @@ from torchvision.utils import make_grid
 
 '''
 Here is the implementation of VGG16, VGG19 - with Keras and
-Resnet50 and Densenet161 with Pytorch. Layers of base model were set to non trainable and then set as trainable to compare the performance of the model.
-There are some visualization methods there as well. Samples visualization was performed only with keras, to omit repetitions, yet augmentation visualization was performed using both keras and torch libraries.
+Resnet50 and Densenet161 with Pytorch. Layers of base model were set to non trainable and then set as trainable to compare the performance of the models.
+There are some visualization methods as well. Samples visualization was performed only with keras, to omit repetitions, yet augmentation visualization was performed using both keras and torch libraries.
 Optimal learning rate for all models was implemented to determine visually and to be set on user choice.(Leslie N. Smith method - https://arxiv.org/abs/1506.01186)
 All visualizations were stored to the corresponding folder(*project_folder/results/**library)
 *location of all of the files for the project
